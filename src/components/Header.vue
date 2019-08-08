241b59013b46a62ffa3971c98c1ed56d50ab2f22
khalid alasmary <khalid@alasmary.com>
2019-08-08 07:58:59 +0300

    finshing...

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <img :src="image" />
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      image: require("@/assets/logo.png")
+    };
+  }
+};
+</script>
+<style scoped>
+img {
+  width: 300px;
+}
+
+@media (max-width: 700px) {
+  img {
+    display: block;
+    margin: auto;
+  }
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="search">
+    <h1>Track Player States</h1>
+    <form v-on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="platform">Platform</label>
+        <select name="palatform" id="platform" v-model="platform">
+          <option value="psn">Playstation</option>
+          <option value="xbl">Xbox</option>
+          <option value="origin">Origin</option>
+        </select>
+      </div>
+      <div class="form-grou">
+        <label for="gamertag">Gamertag</label>
+        <input
+          type="text"
+          name="gamertag"
+          v-model="gamertag"
+          id="gamertag"
+          placeholder="Origin ID, Xbox Live gamertag, PSN ID, ect"
+        />
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Submit" class="btn" />
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      platform: "psn",
+      gamertag: ""
+    };
+  },
+  beforeCreate() {
+    document.body.className = "body-bg-image";
+  },
+  methods: {
+    onSubmit() {
+      if (!this.gamertag) {
+        this.$toasted.show("Please enter a gamertag", {
+          duration: 3000,
+          icon: "exclamation-circle"
+        });
+      } else {
+        this.$router.push(`/profile/${this.platform}/${this.gamertag}`);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
